16758c27530132bc3c6f99f93a3d30e5cda90fea
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-11-06 22:29:09 +0200

    add first class

---
+++ RefTypeValTypeParams/RefTypeValTypeParams/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefTypeValTypeParams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**** Passing Person object by value ****");
+            Person fred = new Person("Fred", 12);
+            Console.WriteLine("\nBefore by value call, Person is:");//Перед вызовом
+            fred.Display();
+
+            SendPersonByValue(fred);
+            Console.WriteLine("\nAfter by value call, Person is:");//После вызова
+            fred.Display();
+            Console.ReadLine();
+        }
+        static void SendPersonByValue(Person p)
+        {
+            p.personAge = 99;
+            p = new Person("Niki", 99);
+        }
+    }
+
+    class Person
+    {
+        public string personName;
+        public int personAge;
+
+        //Конструкторы
+        public Person(string name, int age)
+        {
+            personName = name;
+            personAge = age;
+        }
+        public Person() { }
+        public void Display()
+        {
+            Console.WriteLine("Name: {0}, Age: {1}", personName, personAge);
+        }
+        
+    }
+}
