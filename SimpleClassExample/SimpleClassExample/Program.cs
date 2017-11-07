219edd581b1b1c150a1741ea5ef47e2f43cd09ff
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-11-07 14:36:19 +0200

    add simple class car

---
+++ SimpleClassExample/SimpleClassExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** Fun with Class Types ***** ");
+            //Разместить в память и сконфигурировать объект Car.
+            Car myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+
+            //Увеличить скорость автомобиля в несколько раз и вывести новое состояние
+            for (int i = 0; i <= 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+            Console.ReadLine();
+        }
+    }
+}
