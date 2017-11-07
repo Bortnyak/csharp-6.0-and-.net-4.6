8a26880c3256860b93c5d14b5826fc6af6c87399
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-11-07 16:25:59 +0200

    add some overloaded methods to Motorcycle class

--- SimpleClassExample/SimpleClassExample/Motorcycle.cs
+++ SimpleClassExample/SimpleClassExample/Motorcycle.cs
@@ -9,25 +9,44 @@ namespace SimpleClassExample
     class Motorcycle
     {
         public int driverIntensity;
-        public string name;
-        public void PopAWheely()
-        {
-            for (int i = 0; i <= driverIntensity; i++)
-            {
-                Console.WriteLine("Yeeeeeeee Haaaaaeeewww!");
-            }
-        }
+        public string driverName;
+        
         //Переопределяю стандарный конструктор
         public Motorcycle() { }
 
         //Специалный конструктор
         public Motorcycle(int intensity)
         {
-            driverIntensity = intensity;
+
+            SetIntensity(intensity);
         }
+
+        public Motorcycle(int intensity, string name)
+        {
+            SetIntensity(intensity);
+            driverName = name;
+        }
+
         public void SetDriverName(string name)
         {
-            this.name = name;
+            driverName = name;
+        }
+
+        public void SetIntensity(int intensity)
+        {
+            if (intensity > 10)
+            {
+                intensity = 10;
+            }
+            driverIntensity = intensity;
+        }
+
+        public void PopAWheely()
+        {
+            for (int i = 0; i <= driverIntensity; i++)
+            {
+                Console.WriteLine("Yeeeeeeee Haaaaaeeewww!");
+            }
         }
     }
 }
