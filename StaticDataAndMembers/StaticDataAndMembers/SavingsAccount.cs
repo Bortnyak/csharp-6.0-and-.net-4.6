34426aea255c2693298b507012bdd5dd0a3891dc
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-12-19 19:41:19 +0200

    add SavingsAccount class

---
+++ StaticDataAndMembers/StaticDataAndMembers/SavingsAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class SavingsAccount
+    {
+        public double currBalance;
+        public SavingsAccount(double balance)
+        {
+            currBalance = balance;
+        }
+
+        //Статический элемент данных.
+        public static double currentInterestRate = 0.04;
+    }
+}
