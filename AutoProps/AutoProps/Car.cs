9275577c118dc2916d9484db372c761f8e18201a
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-12-20 22:53:19 +0200

    add car class in new prjct, which named AutoProps

---
+++ AutoProps/AutoProps/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProps
+{
+    class Car
+    {
+        //Автоматические свойства. Нет нужды в определении поддерживающих полей.
+        public string PetName { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+        //public int MyReadOnlyProp { get;}
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine("Car Name: {0}", PetName);
+            Console.WriteLine("Speed: {0}", Speed);
+            Console.WriteLine("Color: {0}", Color);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AutoProps/AutoProps/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****  Fun with Automatic Properties  *****\n");
+            Car macQuin = new Car();
+            macQuin.PetName = "Lightning";
+            macQuin.Speed = 89;
+            macQuin.Color = "Red";
+
+            macQuin.DisplayStatus();
+        }
+    }
+}
