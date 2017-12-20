46514851eab08db9a55d00a1ce0f4b0b1755c749
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-12-20 17:29:58 +0200

    add simple static class

---
+++ SimpleUtilityClass/SimpleUtilityClass/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleUtilityClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Функциональность на уровне класса
+            //Это работает нормально!
+            TimeUtilClass.PrintDate();
+            TimeUtilClass.PrintTime();
+
+            Console.ReadLine();
+        }
+    }
+}
