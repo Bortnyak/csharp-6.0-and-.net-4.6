87ee40b56769bab870c866a4d77c56af102250dc
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-12-27 13:43:12 +0200

    add BasicInheritance prjct

---
+++ BasicInheritance/BasicInheritance/MiniVan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    //MiniVan "является" Car.
+    sealed class MiniVan : Car
+    {
+        
+    }
+}
---
+++ BasicInheritance/BasicInheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** Basic Inheritance ***** ");
+
+            Car myCar = new Car(80);
+            myCar.Speed = 50;
+            myCar.DisplayStatus();
+
+            Car mySecondCar = new Car(120);
+            mySecondCar.Speed = 121;
+            mySecondCar.DisplayStatus();
+
+            MiniVan myMiniVan = new MiniVan();
+            myMiniVan.Speed = 10;
+            Console.WriteLine("My van is going {0} MPH", myMiniVan.Speed);
+
+            Console.ReadLine();
+        }
+    }
+}
