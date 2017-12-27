5e6356bd3ab8bffa5a4e2eef57d65e5fea8e94cb
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-12-27 12:57:51 +0200

    add ConstData Class

---
+++ ConstData/ConstData/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** Fun with Const ***** ");
+            Console.WriteLine("The value of PI is {0}", MyMathClass.PI);
+
+            //Ошибка, константу изменять нельзя.
+            //MyMathClass.PI = 3.14;
+            Console.ReadLine();
+        }
+    }
+}
