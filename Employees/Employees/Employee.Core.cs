b000ce12ead87330bfd31b335ada0007310961b3
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-12-27 18:17:53 +0200

    add some class for employees

---
+++ Employees/Employees/Employee.Core.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    partial class Employee
+    {
+        // Field data.
+        protected string empName;
+        protected int empID;
+        protected float currPay;
+        protected int empAge;
+        protected string empSSN = "";
+        protected BenefitPackage empBenefits = new BenefitPackage();
+
+        #region Ctors
+        // Note use of constructor chaining.
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+          : this(name, 0, id, pay)
+        { }
+        public Employee(string name, int age, int id, float pay)
+        {
+            Name = name;
+            Age = age;
+            ID = id;
+            Pay = pay;
+        }
+        public Employee(string name, int age, int id, float pay, string ssn)
+            :this (name, age, id, pay)
+        {
+            empSSN = ssn;
+        }
+        #endregion
+
+        #region Properties 
+        // Properties!
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error!  Name length exceeds 15 characters");
+                else
+                    empName = value;
+            }
+        }
+
+        // We could add additional business rules to the sets of these properties,
+        // however there is no need to do so for this example.
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
+        public string SocialSecurityNumber
+        {
+            get { return empSSN; }
+        }
+        public BenefitPackage Benefits
+        {
+            get { return empBenefits; }
+            set { empBenefits = value; }
+        }
+        #endregion
+    }
+}
---
+++ Employees/Employees/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** The Employee Class Hierachy ***** \n");
+            //SalesPerson fred = new SalesPerson();
+            //fred.Age = 31;
+            //fred.Name = "Fred";
+            //fred.SalesNumber = 50;
+
+            //Employee emp = new Employee();
+            //emp.Name = "Nick";
+
+            Manager chucky = new Manager("Chucky", 50, 92, 10000, "333-23-32", 9000);
+            double cost = chucky.GetBenefitCoast();
+            Console.ReadLine();
+        }
+    }
+}
