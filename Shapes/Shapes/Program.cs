a0c8c14b0d7393539338e73a2a83e8680776ec5d
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2017-12-28 18:59:54 +0200

    add Shapes project(Chapter_6_Troelsen)

---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** Fun with Polimorphism ***** \n");
+
+            Shape[] myShapes = { new Hexagon(), new Circle(), new Hexagon("Mick"),
+            new Circle("Beth"), new Hexagon("Linda")};
+            foreach (Shape s in myShapes)
+            {
+                s.Draw();
+            }
+            Console.ReadLine();
+
+            ThreeDCircle o = new ThreeDCircle();
+            o.Draw();
+
+            ((Circle)o).Draw();
+            Console.ReadLine();
+        }
+    }
+}
