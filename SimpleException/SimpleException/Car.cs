d0f7d08886af7956333c09c55c1cc8520405e728
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2018-01-04 19:01:34 +0200

    add SimpleException project

---
+++ SimpleException/SimpleException/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        //Костанта для представления максимальной скорости.
+        public const int maxSpeed = 100;
+        
+        //Свойства автомобиля.
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        //Не вышел ли автомобил из строя?
+        private bool carIsDead;
+
+        //В автомобиле имеется радиоприемник.
+        private Radio theMusicBox = new Radio();
+
+        //Констукторы
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public void CrankTunes(bool state)
+        {
+            //Делегировать запрос внутреннему объекту.
+            theMusicBox.TurnOn(state);
+        }
+
+        //Проверить, не перегрелся ли автомобиль.
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                Console.WriteLine("{0} is out of order..", PetName);
+            }
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed >= maxSpeed)
+                {
+                    carIsDead = true;
+                    CurrentSpeed = 0;
+
+                    //Использовать ключевое слово throw для генерации исключения.
+                    throw new Exception(string.Format("{0} has overheated!", PetName));
+                }
+                else
+                {
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+                }
+            }
+        }
+
+    }
+}
---
+++ SimpleException/SimpleException/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***** Simple Exception Example ***** ");
+            Console.WriteLine("=> Creating a car and stepping on it!");
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+
+            //Розогнать автомобиль до скорости, превышающей максимальный предел, с целью выдачи исключения.
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    myCar.Accelerate(10);
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("\n*** Error! ***");
+                Console.WriteLine("Method: {0}", e.TargetSite); //метод
+                Console.WriteLine("Message: {0}", e.Message); //сообщение
+                Console.WriteLine("Source: {0}", e.Source); //источник
+            }
+            Console.WriteLine("\n***** Out of Exception logic *****");
+            Console.ReadLine();
+        }
+    }
+}
