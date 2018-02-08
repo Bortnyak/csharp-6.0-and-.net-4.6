98cc1435aa8105144e0e159cac755b3c2dd74f0b
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2018-02-08 20:50:29 +0200

    add ICloneableExample

---
+++ ICloneableExample/ICloneableExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICloneableExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** A First Look at Interface *****\n");
+
+            //Все эти классы поддерживают интерфейс ICloneable.
+            string myStr = "Hello";
+            OperatingSystem unixOS = new OperatingSystem(PlatformID.Unix, new Version());
+            System.Data.SqlClient.SqlConnection sqlCnn = new System.Data.SqlClient.SqlConnection();
+
+            //Следовательно, все они могут буть переданы методу, принимающему параметр типа ICloneable.
+            CloneMe(myStr);
+
+            ICloneable myObj = "123";
+            object myObjClone = myObj.Clone();
+            Console.WriteLine("myObj is {0},\n myObjClone is {1}", myObj, myObjClone);
+        }
+        private static void CloneMe(ICloneable c)
+        {
+            // Клонировать то, что получено, и вывести его имя.
+            object theClone = c.Clone();
+            Console.WriteLine("You clone is a: {0}", theClone.GetType().Name);
+
+           
+        }
+    }
+}
