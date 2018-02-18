5b8516e1a8e0339bd1811d3e64505212486b1f18
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2018-02-18 18:41:39 +0200

    add some interface to Shapes

---
+++ CustomInterface/CustomInterface/IPointy.cs
@@ -0,0 +1,8 @@
+﻿namespace CustomInterface
+{
+    internal interface IPointy
+    {
+        //Неявно открытый и абстрактный
+        byte Points { get;  }
+    }
+}
--- ICloneableExample/ICloneableExample/Program.cs
+++ ICloneableExample/ICloneableExample/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ICloneableExample
 {
@@ -29,8 +25,6 @@ namespace ICloneableExample
             // Клонировать то, что получено, и вывести его имя.
             object theClone = c.Clone();
             Console.WriteLine("You clone is a: {0}", theClone.GetType().Name);
-
-           
         }
     }
 }
