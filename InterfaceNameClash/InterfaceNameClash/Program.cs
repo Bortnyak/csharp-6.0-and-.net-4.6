c5c24b7f19802cdaffe25f4eeea33a4b1ae2e93e
Bortnyak <inf1b14.bortnyak@kpnu.edu.ua>
2018-02-20 20:10:25 +0200

    add some interface to InterfaceNameClash

---
+++ InterfaceNameClash/InterfaceNameClash/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Octagon oct = new Octagon();
+            //Для доступа к членам Draw() должно использоваться приведение.
+            IDrawToForm itfForm = (IDrawToForm)oct;
+            itfForm.Draw();
+
+            //Сокращенная форма, если переменная интерфейса не нужна.
+            ((IDrawToMemory)oct).Draw();
+        }
+    }
+}
